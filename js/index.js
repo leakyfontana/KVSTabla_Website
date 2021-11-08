26baabda012205f8d90c4a76c27e8baf92779c3e
Xander Dyer <xander.dyer@hotmail.com>
2021-11-08 09:14:51 -0500

    Adding collapsible form and carousel

--- js/index.js
+++ js/index.js
@@ -10,3 +10,18 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
