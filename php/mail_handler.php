3ebcb1884ee74e1f068db55eb817ad8ab4a1b5f3
Xander Dyer <xander.dyer@hotmail.com>
2021-11-13 15:57:07 -0500

    Adding images + php and css gird

---
+++ php/mail_handler.php
@@ -0,0 +1,24 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "xdyer777@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $event = $_POST['event'];
+    $location = $_POST['location'];
+    $details = $_POST['details'];
+
+    $subject = " New KVSTabla booking request";
+    $subject2 = "Copy of your request to book KVSTabla";
+    $message = $first_name . " " . $last_name . " has requested to book you for a(n) " . $event . "in the" . $location . "area." . "\n\n Additional details: " . $details;
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $message;
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
